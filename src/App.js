f448b4a2d1db2f2a4e0e023d91313bb28a3d07ed
Antonio <tonijorda1997@gmail.com>
2021-08-30 07:17:46 +0200

    Header styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React from "react";
+
+//components
+import Header from './components/Header';
 
 
+//styles
+import { GlobalStyle } from "./GlobalStyle";
 
 function App() {
-  return (
-    <div className="App">
-      Start here.
+  return (<div 
+    className="App">
+      <Header />
+    Start here.
+    <GlobalStyle />
     </div>
   );
 }
