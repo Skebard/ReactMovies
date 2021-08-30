2ace6d736d1c5aa68f3d4988610dd115071197e7
Antonio <tonijorda1997@gmail.com>
2021-08-30 21:30:50 +0200

    Display main movie

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,24 +1,34 @@
 import React, { useState, useEffect } from "react";
 
 //API
-import API from '../API';
+import API from "../API";
 
 //config
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from "../config";
 
 //components
+import HeroImage from '../components/HeroImage';
 
 //hook
-import { useHomeFetch } from '../hooks/useHomeFetch';
+import { useHomeFetch } from "../hooks/useHomeFetch";
 
 //images
 import NoImage from "../images/no_image.jpg";
 
 const Home = () => {
-
-    const {state, loading, error } = useHomeFetch();
-    console.log(state);
-    return <div> Home page </div>;
+  const { state, loading, error } = useHomeFetch();
+  console.log(state);
+  return (
+    <>
+      {state.results[0] && (
+        <HeroImage
+          image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
+          title={state.results[0].original_title}
+          text={state.results[0].overview}
+        />
+      )}
+    </>
+  );
 };
 
 export default Home;
