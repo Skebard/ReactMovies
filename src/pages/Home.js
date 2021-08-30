0b824fc7b1328f42ff7ac06d78f5a5ca448fe7b5
Antonio <tonijorda1997@gmail.com>
2021-08-30 20:59:14 +0200

    Fetch movie data

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,37 +9,14 @@ import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from "../config";
 //components
 
 //hook
+import { useHomeFetch } from '../hooks/useHomeFetch';
 
 //images
 import NoImage from "../images/no_image.jpg";
 
 const Home = () => {
-    const [state, setState] = useState();
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(false);
 
-    const fetchMovies = async(page, searchTerm = "") => {
-        try{
-            setError(false);
-            setLoading(true);
-
-            const movies = await API.fetchMovies(searchTerm, page);
-            setState((prev) => ({
-                ...movies,
-                results:
-                    page > 1 ? [...prev.results, ...movies.results] : [...movies.results]
-            }));
-
-            setLoading(false);
-        }catch(error){
-            setError(true);
-        }
-    }
-
-    useEffect(() => {
-        fetchMovies(1);
-    }, [])
-
+    const {state, loading, error } = useHomeFetch();
     console.log(state);
     return <div> Home page </div>;
 };
