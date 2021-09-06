f8940fb066d3ea92588981805a3be53ba540559f
Antonio <tonijorda1997@gmail.com>
2021-09-06 04:27:15 +0200

    Searh bar styles

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,3 @@
-import React, { useState, useEffect } from "react";
-
 //API
 import API from "../API";
 
@@ -8,6 +6,10 @@ import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from "../config";
 
 //components
 import HeroImage from '../components/HeroImage';
+import Grid from '../components/Grid';
+import Thumb from '../components/Thumb';
+import Spinner from '../components/Spinner';
+import SearchBar from '../components/SearchBar';
 
 //hook
 import { useHomeFetch } from "../hooks/useHomeFetch";
@@ -16,7 +18,7 @@ import { useHomeFetch } from "../hooks/useHomeFetch";
 import NoImage from "../images/no_image.jpg";
 
 const Home = () => {
-  const { state, loading, error } = useHomeFetch();
+  const { state, loading, error, setSearchTerm } = useHomeFetch();
   console.log(state);
   return (
     <>
@@ -27,6 +29,22 @@ const Home = () => {
           text={state.results[0].overview}
         />
       )}
+      <SearchBar setSearchTerm={setSearchTerm} />
+      <Grid header="Popular Movies">
+          {state.results.map(movie => (
+              <Thumb
+                key={movie.id}
+                clickable={true}
+                image={
+                  movie.poster_path
+                  ? IMAGE_BASE_URL + POSTER_SIZE + movie.poster_path
+                  : NoImage
+                }
+                movieId={movie.id}
+                />
+          ))}
+      </Grid>
+      <Spinner />
     </>
   );
 };
