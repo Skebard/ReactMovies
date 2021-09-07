56c5a03e3e9f35946bbdfdd0c5c84fc4b24d46bd
Antonio <tonijorda1997@gmail.com>
2021-09-07 06:21:53 +0200

    Movie

---
+++ src/components/Movie.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import { IMAGE_BASE_URL, POSTER_SIZE } from "../config";
+
+import Grid from "./Grid";
+import Spinner from "./Spinner";
+import BreadCrumb from "./BreadCrumb";
+import MovieInfo from "./MovieInfo";
+import MovieInfoBar from "./MovieInfoBar";
+
+import { useMovieFetch } from "../hooks/useMovieFetch";
+
+import NoImage from "../images/no_image.jpg";
+
+const Movie = () => {
+  const { movieId } = useParams();
+  const { state: movie, loading, error } = useMovieFetch(movieId);
+
+  if (loading) {
+    return <Spinner />;
+  }
+  if (error) {
+    return <div> Something went wrong... </div>;
+  }
+
+  console.log(movie);
+  return (
+    <>
+      <BreadCrumb movieTitle={movie.original_title} />
+      <MovieInfo movie={movie} />
+      <MovieInfoBar
+        time={movie.runtime}
+        budget={movie.budget}
+        revenue={movie.revenue}
+      />
+    </>
+  );
+};
+
+export default Movie;
